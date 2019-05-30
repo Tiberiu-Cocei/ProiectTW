bb0be9ace056df3cf5526ed9e8e6916b6867119a
BeatriceB123 <48123183+BeatriceB123@users.noreply.github.com>
2019-05-30 21:30:10 +0300

    .

--- pages/generate_password.php
+++ pages/generate_password.php
@@ -27,14 +27,6 @@
     </div>
     <div class="text-center center">
         <BR><BR><BR><BR>
-    
-        <!--
-            <h2 class="form-control dataField password-generated" id="safePassword"> Password goes here </h2>
-            <h2 style="text-align: center; margin-left:75px; margin-top: 125px;" class="form-control dataField" id="ASDASD"> Password goes here</h2>
-            
-            alert('Hello world!')
-            
-            -->
      <form>     
      <h3> 
         <?php 
@@ -42,11 +34,6 @@
             echo  $generatedPassword; 
         $_SESSION['generatedPassword'] = generateSafePassword(); ?> 
      </h3>
-
-        <!-- <button onclick = "alert('Hello world!')" id="generatePassword" type="button"
-                style="margin-top:35px;"
-                class="button middle innerButton"><b>Generate safe password</b></button> -->
-
         <input type="button" value="Generate safe password" class="button middle innerButton" id="generatePassword" style="margin-top:35px;"
             onClick="document.location.href='./generate_password.php'" />
 
