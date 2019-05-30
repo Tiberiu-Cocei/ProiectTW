a70cbf8829fd1aecef846a1400d165d445358e33
BeatriceB123 <48123183+BeatriceB123@users.noreply.github.com>
2019-05-30 14:07:22 +0300

    generate password - integrare

--- pages/generate_password.php
+++ pages/generate_password.php
@@ -1,3 +1,16 @@
+<?php
+  session_start();
+
+  include '../api/algorithms/safe_password.php';
+  function regeneratePassword(){
+     // echo "HERE"; 
+     $_SESSION['generatedPassword'] = generateSafePassword(); 
+  }
+  $_SESSION['generatedPassword'] = generateSafePassword(); 
+  $generatedPassword  =  generateSafePassword(); 
+  //$generatedPassword = "parola"; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,17 +26,33 @@
     <div class="text-center">
     </div>
     <div class="text-center center">
-        <h2 class="form-control dataField password-generated" id="safePassword">
-            Password goes here
-        </h2>
+        <BR><BR><BR><BR>
+    
         <!--
+            <h2 class="form-control dataField password-generated" id="safePassword"> Password goes here </h2>
             <h2 style="text-align: center; margin-left:75px; margin-top: 125px;" class="form-control dataField" id="ASDASD"> Password goes here</h2>
-        -->
-        <button onclick="location.href = '#generatePassword';" id="generatePassword" type="button"
+            
+            alert('Hello world!')
+            
+            -->
+     <form>     
+     <h3> 
+        <?php 
+        if( isset( $_SESSION['generatedPassword']) && $_SESSION['generatedPassword'] != null) 
+            echo  $generatedPassword; 
+        $_SESSION['generatedPassword'] = generateSafePassword(); ?> 
+     </h3>
+
+        <!-- <button onclick = "alert('Hello world!')" id="generatePassword" type="button"
                 style="margin-top:35px;"
-                class="button middle innerButton"><b>Generate safe password</b></button>
+                class="button middle innerButton"><b>Generate safe password</b></button> -->
+
+        <input type="button" value="Generate safe password" class="button middle innerButton" id="generatePassword" style="margin-top:35px;"
+            onClick="document.location.href='./generate_password.php'" />
+
         <button onclick="location.href = 'main_page.php';" id="return" type="button"
                 class="button middle innerButton"><b>Return</b></button>
+    </form>
     </div>
 </div>
 </body>
--- pages/main_page.php
+++ pages/main_page.php
@@ -16,13 +16,15 @@
 <body>
 <nav>
     <ul class="menuBar">
-        <li class="myAccount"><a>Signed in as: <?php echo $_SESSION['username']; ?></a></li>
-        <li class="changePassword"><a href="./account/changePassword.php">Change password</a></li>
-        <li class="exportData"><a href="export.php">Export data</a></li>
-        <li class="generatePassword"><a href="./generate_password.php">Generate safe password</a></li>
-        <li class="logout"><a href="./account/logout.php">Logout</a></li>
+        <li class="myAccount">       <a style="color:#f6cd61;">                                    Signed in as: <?php echo $_SESSION['username']; ?></a></li>
+        <li class="changePassword">  <a style="color:#f6cd61;" href="./account/changePassword.php">Change password                                   </a></li>
+        <li class="exportData">      <a style="color:#f6cd61;" href="export.php">                  Export data                                       </a></li>
+        <li class="generatePassword"><a style="color:#f6cd61;" href="./generate_password.php">     Generate safe password                            </a></li>
+        <li class="logout">          <a style="color:#f6cd61;" href="./account/logout.php">        Logout                                            </a></li>
     </ul>
 </nav>
+
+
 
 <div class="grid-container">
     <div class="center column" src="submain.php">
@@ -30,6 +32,7 @@
               class="buttonReversed middle innerButton"><b>Add new category</b></button>
       <button type="button" id="Frequency" style="margin-top:20px" class="buttonReversed middle innerButton"><b>Accounts by
           use frequency</b></button>
+          
       <button type="button" id="Strength" class="buttonReversed middle innerButton"><b>Accounts by password strength</b>
       </button>
       <button type="button" class="buttonReversed middle innerButton"><b>Categories</b></button>
@@ -44,6 +47,13 @@
       <button onclick="location.href = '#Others';" id="Others" type="button" class="button middle innerButton"><b>Others</b>
       </button>
     </div>
+
+
+
+
+
+
+
     <div class="center column" src="accounts.php">
       <button onclick="location.href = 'new_account.php';" id="addSite" type="button" class="buttonReversed middle innerButton"><b>Add new account</b></button>
 
