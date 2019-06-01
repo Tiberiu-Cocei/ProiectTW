50135b089220dec196a7ff55a37e2b70bb6bc2e5
BeatriceB123 <48123183+BeatriceB123@users.noreply.github.com>
2019-06-01 09:13:01 +0300

    afisarea conturilor din categoria selectata
    //REV
    Obs1: daca numele categoriei contine spatii atunci nu se afiseaza nimic.
    E posibil sa fie de pe la afisare problema asta.
    Obs2: o categorie fara conturi asociate da "Connection Failure".

--- pages/main_page.php
+++ pages/main_page.php
@@ -1,27 +1,18 @@
 <!DOCTYPE html>
 <?php
+  include_once '../includes/apiCall.php';
   if(!isset($_SESSION))
   {
       session_start();
   }
   if($_SESSION['username'] === null) header("Location:./Login.php");
 
+
   function echoCategoryButton($category_name)
   {
-    // $buttonSettings = "<button onclick=\" \$_SESSION['current_category'] = \"CURRENT\" ; location.href = '#"
-    //                   .$category_name
-    //                   ."';\""
-    //                   ."id=\""
-    //                   .$category_name
-    //                   ."\" "
-    //                   ."type=\"button\" class=\"button middle innerButton\">"
-    //                   ."<b>".$category_name."</b></button>"; 
-    $_SESSION['current_category'] = "CATEGORIA HARD"; 
-
-    $buttonSettings = "<input type=\"button\" value=\"$category_name\" 
-    class=\"button middle innerButton\"
-    onClick=\"document.location.href='./main_page.php'\" </input><br>"; 
-
+    $buttonSettings = "<input type=\"submit\" name=\"$category_name\" value=\"$category_name\"
+    class=\"button middle innerButton\"><br>"; 
+    //onClick=\"document.location.href='./main_page.php'\"
     echo $buttonSettings;
   }
 
@@ -48,7 +39,7 @@
     echo $details; 
   }
 
-  function getAccounts($orderType)
+  function getAccounts($orderType, $allCategories = array())
   {
     if($orderType == 'strength' || $orderType == 'usage')
     {
@@ -56,119 +47,145 @@
   
       $make_call = ApiCall('GET', $accountsApi, json_encode($_SESSION['id_utilizator']));
 
-      $response = json_decode($make_call, true);
+      return json_decode($make_call, true);
     }
-    else 
+    else if($orderType = 'justSelectedCategory')
     {
-      $response['records'] = array();
-    } 
+      foreach($allCategories as $key_category_name => $value_category_id) 
+      {
+        if(isset($_POST[$key_category_name]))
+        {
+          return getAccountsByCategory($value_category_id);
+        }
+      }
+    }
+    //if($orderType == 'none')
+    $response['records'] = array();
+
     return $response;
   }
 
+  function getAccountsByCategory($id_categorie)
+  {
+    $accountsApi = 'http://localhost/api/account/get_by_category.php?id_categorie='.$id_categorie; 
 
+    $make_call = ApiCall('GET', $accountsApi, json_encode($id_categorie));
 
+    return json_decode($make_call, true);
+  }
 
+  function isACategorySelected($allCategories)
+  {
+    foreach($allCategories as $key_category_name => $value_category_id) 
+    {
+      if(isset($_POST[$key_category_name]))
+      {
+        return true;
+      }
+    }
+    return false; 
+  }
 
 ?>
+
 <html lang="en-US">
-
 <head>
-    <meta charset="utf-8">
-    <title>Main page</title>
-    <link rel="stylesheet" href="../public/css/main_page.css">
-    <link rel="icon" type="image/png" sizes="32x32" href="../public/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="../public/images/favicon/favicon-96x96.png">
+  <meta charset="utf-8">
+  <title>Main page</title>
+  <link rel="stylesheet" href="../public/css/main_page.css">
+  <link rel="icon" type="image/png" sizes="32x32" href="../public/images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="../public/images/favicon/favicon-96x96.png">
 </head>
-
 <body>
-<nav>
-    <ul class="menuBar">
-        <li class="myAccount">       <a style="color:#f6cd61;">                                    Signed in as: <?php echo $_SESSION['username']; ?></a></li>
-        <li class="changePassword">  <a style="color:#f6cd61;" href="./account/changePassword.php">Change password                                   </a></li>
-        <li class="exportData">      <a style="color:#f6cd61;" href="export.php">                  Export data                                       </a></li>
-        <li class="generatePassword"><a style="color:#f6cd61;" href="./generate_password.php">     Generate safe password                            </a></li>
-        <li class="logout">          <a style="color:#f6cd61;" href="./account/logout.php">        Logout                                            </a></li>
+<nav><BR>
+  <ul class="menuBar">
+    <li class="myAccount">       <a style="color:#f6cd61; font-weight: bold;">                                    Signed in as: <?php echo $_SESSION['username']; ?></a></li>
+    <li class="changePassword">  <a style="color:#f6cd61; font-weight: bold;" href="./account/changePassword.php">Change password                                   </a></li>
+    <li class="exportData">      <a style="color:#f6cd61; font-weight: bold;" href="export.php">                  Export data                                       </a></li>
+    <li class="generatePassword"><a style="color:#f6cd61; font-weight: bold;" href="./generate_password.php">     Generate safe password                            </a></li>
+    <li class="logout">          <a style="color:#f6cd61; font-weight: bold;" href="./account/logout.php">        Logout                                            </a></li>
     </ul>
 </nav>
 
 <div class="grid-container">
-    <div class="center column" src="submain.php">
-    <form action="main_page.php" method="post">
-        <button onclick="location.href = 'new_category.php';" id="addCategory" type="button" class="buttonReversed middle innerButton"><b>Add new category</b></button>
-        <button name="usageOrder"      type="submit" id="Usage"      class="buttonReversed middle innerButton"  style="margin-top:20px"><b>Accounts by usage</b></button>
-        <button name="strengthOrder"   type="submit" id="Strength"   class="buttonReversed middle innerButton">                         <b>Accounts by password strength</b></button>
-        <!-- <button name="usageOrder"      type="submit"                 class="buttonReversed middle innerButton">                         <b>Accounts by usage</b></button> -->
-        <button name="showCategories"  type="submit"                 class="buttonReversed middle innerButton">                         <b>Categories</b></button>
-      </form>
+  <div class="center column" src="submain.php">
+  <form action="main_page.php" method="post">
+    <button onclick="location.href = 'new_category.php';" id="addCategory" type="button" class="buttonReversed middle innerButton"><b>Add new category</b></button>
+    <button name="usageOrder"      type="submit" id="Usage"      class="buttonReversed middle innerButton"  style="margin-top:20px"><b>Accounts by usage</b></button>
+    <button name="strengthOrder"   type="submit" id="Strength"   class="buttonReversed middle innerButton">                         <b>Accounts by password strength</b></button>
+    <!-- <button name="usageOrder"      type="submit"                 class="buttonReversed middle innerButton">                         <b>Accounts by usage</b></button> -->
+    <button name="showCategories"  type="submit"                 class="buttonReversed middle innerButton">                         <b>Categories</b></button>
+  </form>
 
-      <?php
-      include_once '../includes/apiCall.php';
-      $_SESSION['current_category'] = "";
+  <?php
+//in acest bloc de php extragem toate denumirile conturilor pentru utiizatorul conectat (pentru afisare a butoanelor de categorii - cu functionalitate)
+  $userApi = 'http://localhost/TWPM/api/user/get_by_name.php?username='.$_SESSION['username']; 
 
-      $userApi = 'http://localhost/TWPM/api/user/get_by_name.php?username='.$_SESSION['username']; 
+  $make_call = ApiCall('GET', $userApi, json_encode($_SESSION['username']));
 
-      $make_call = ApiCall('GET', $userApi, json_encode($_SESSION['username']));
+  $response = json_decode($make_call, true);
 
-      //echo $make_call;
+  $data     = $response['id_utilizator'];
 
-      $response = json_decode($make_call, true);
+  if($data == "Could not find any user with given username." || $data == null)
+  {
+      header("Location: ./Login.php");
+  }
+  else
+  {
+    $_SESSION['id_utilizator'] = $data; 
+    $categoriesApi = 'http://localhost/api/category/get_by_user_id.php?id_utilizator='.$data; 
 
-      $data     = $response['id_utilizator'];
+    $make_call = ApiCall('GET', $categoriesApi);
 
+    $response = json_decode($make_call, true);
 
-      if($data == "Could not find any user with given username." || $data == null)
-      {
-          header("Location: ./Login.php");
-      }
-      else
-      {
-        $_SESSION['id_utilizator'] = $data; 
-        $categoriesApi = 'http://localhost/api/category/get_by_user_id.php?id_utilizator='.$data; 
+    $allCategories = array(); 
 
-        $make_call = ApiCall('GET', $categoriesApi);
+    echo "<form method=\"POST\">";  
+    foreach($response['records'] as $category) {
+      echoCategoryButton( $category['nume_categorie'] );                            //adaugam butonul categoriei 
+      $allCategories += [$category['nume_categorie'] => $category['id_categorie']]; //punem numele ( categoriei + id ) intr-o variabila
+    }
+  }
+  ?>
+</div>
 
-        $response = json_decode($make_call, true);
+<!-- de aici incepe coloana a doua !!!!!!!!! -->
+<div class="center column" src="accounts.php">
+  <?php 
+  //verificam daca suntem in cadrul unei categorii si putem adauga butonul de adaugare a unui cont
+  if(isACategorySelected($allCategories))
+  {
+  ?>
+  <button onclick="location.href = 'new_account.php';" id="addSite" type="button" class="buttonReversed middle innerButton"><b>Add new account</b></button>
+  
+  <?php
+  ;}
+  
+  //in cazul in care s-a cerut, afisam conturile, filtrate corespunzator
+  if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['usageOrder']))
+  {
+    $_SESSION['canAddAccount'] = true; 
+    $accounts = getAccounts('usage');  
+  }
+  else if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['strengthOrder']))
+  {
+    $_SESSION['canAddAccount'] = true;
+    $accounts = getAccounts('strength');
+  }
+  else 
+  {
+    $_SESSION['canAddAccount'] = true; 
+    $accounts = getAccounts('justSelectedCategory', $allCategories); 
+  }
 
-        foreach($response['records'] as $category) {
-          //print_r( $category['nume_categorie']."<br>" ) ;
-          echoCategoryButton( $category['nume_categorie'] );
-        }
-      }
-
-      ?>
-
-      <!-- <button onclick="displayCategory()">Display all accounts</button> -->
-    </div>
-
-      <div class="center column" src="accounts.php">
-        <?php 
-        if(isset($_SESSION['canAddAccount']) && $_SESSION['canAddAccount'] == true)
-        {
-        ?>
-        <button onclick="location.href = 'new_account.php';" id="addSite" type="button" class="buttonReversed middle innerButton"><b>Add new account</b></button>
-        
-        <?php
-            ;}
-        
-          if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['usageOrder']))
-          {
-            $accounts = getAccounts('usage');  
-          }
-          else if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['strengthOrder']))
-          {
-            $accounts = getAccounts('strength');
-          }
-          else
-          {
-            $accounts = getAccounts('none');
-          }
-
-          foreach($accounts['records'] as $account) { 
-            echoAccount($account);
-          }
-        ?>
-       
-    </div> <!--accounts-->
+  foreach($accounts['records'] as $account) { 
+    echoAccount($account);
+  }
+  ?>
+  
+  </div> <!--accounts-->
 </div> <!-- grid container -->
 
 </body>
--- pages/new_category.php
+++ pages/new_category.php
@@ -19,49 +19,59 @@
         <form method="POST">
             <h3>
             <input type="text" name="newCategory" placeholder="category name" class="form-control dataField" style="margin-top:35px;"></h3>
-            <input type="submit" name="tryToCreate" value="Create new category" style="font-weight: bold;" type="button" class="button middle innerButton">
+            <input type="submit" name="tryToCreate" value="Create new category" style="font-weight: bold;" class="button middle innerButton">
             <button onclick="location.href = 'main_page.php';" type="button" class="button middle innerButton"><b>Cancel</b></button>
         </form>
 
     <?php
-        if(isset($_POST['newCategory']) && ($_POST['newCategory']) != null ){
-            $category_name = $_POST['newCategory']; 
-
-            //scoatem toate categoriile existente din BD ( Aici poate ar fi mai bine sa avem o lista globala cu toate categoriile ca sa nu facem accesari asa dese la bd)
-            $categoriesApi = 'http://localhost/api/category/get_by_user_id.php?id_utilizator='.$_SESSION['id_utilizator']."'"; 
-
-            $make_call = ApiCall('GET', $categoriesApi, json_encode($_SESSION['id_utilizator']));
-
-            $response = json_decode($make_call, true);
-
-            //verificam daca a noastra e existenta in BD sau e noua
-            $inBD = false; 
-            foreach($response['records'] as $category) {
-                if ($category_name == $category['nume_categorie']){
-                    $inBD = true; 
-                }
+    if(isset($_POST['tryToCreate']))
+    {
+        if(!isset($_POST['newCategory']) || ($_POST['newCategory']) == null )
+            echo "<h3>Enter a category name</h3>";
+        else
+        {
+            if(!ctype_alnum (str_replace(' ', 'A', $_POST['newCategory']))) //alfanum or contains spaces
+            {
+                echo "<h3>Choose alfa-numeric name for your category</h3>"; 
             }
+            else
+            {
+                $category_name = $_POST['newCategory']; 
 
-            if($inBD){
-                echo "<h3>Existent category name"; 
-            }//daca nu e in BD incercam sa o adaugam
-            else{
-                
-                $categoriesApi = 'http://localhost/api/category/create.php'; 
-                $arguments =  array(
-                    "id_utilizator"  => $_SESSION['id_utilizator'],
-                    "nume_categorie" => $category_name
-                  );
-                $make_call = ApiCall('POST', $categoriesApi, json_encode($arguments));
+                //scoatem toate categoriile existente din BD ( Aici poate ar fi mai bine sa avem o lista globala cu toate categoriile ca sa nu facem accesari asa dese la bd)
+                $categoriesApi = 'http://localhost/api/category/get_by_user_id.php?id_utilizator='.$_SESSION['id_utilizator']."'"; 
+
+                $make_call = ApiCall('GET', $categoriesApi, json_encode($_SESSION['id_utilizator']));
 
                 $response = json_decode($make_call, true);
 
-                echo "<h3>".$response['message']."</h3>"; 
+                //verificam daca a noastra e existenta in BD sau e noua
+                $inBD = false; 
+                foreach($response['records'] as $category) {
+                    if ($category_name == $category['nume_categorie']){
+                        $inBD = true; 
+                    }
+                }
+
+                if($inBD){
+                    echo "<h3>Existent category name"; 
+                }//daca nu e in BD incercam sa o adaugam
+                else
+                {
+                    $categoriesApi = 'http://localhost/api/category/create.php'; 
+                    $arguments =  array(
+                        "id_utilizator"  => $_SESSION['id_utilizator'],
+                        "nume_categorie" => $category_name
+                    );
+                    $make_call = ApiCall('POST', $categoriesApi, json_encode($arguments));
+
+                    $response = json_decode($make_call, true);
+
+                    echo "<h3>".$response['message']."</h3>"; 
+                }
             }
-        }else{
-            if(isset($_POST['tryToCreate']))
-                echo "<h3>Enter a category name</h3>"; 
         }
+    } 
     ?>
 </div>
 </body>
